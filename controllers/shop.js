5f4a06f70c8dcb34b385c200767f5879949b4d51
Lasha Kitia <lasha.kitia@gmail.com>
2023-12-01 19:03:25 +0400

    chore: handled bad product id with next

--- controllers/shop.js
+++ controllers/shop.js
@@ -14,6 +14,8 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
+    if (!product) return next();
+
     res.render('shop/product-detail', {
       product: product,
       pageTitle: product.title,
