9ecfec6c044f1975e26b2fb498795134554c295d
Davit Kasradze <davit.kasradze23@gmail.com>
2023-12-12 18:27:34 +0400

    175: Problem with orders page

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const sequelize = require('../util/database')
 
 const User = sequelize.define('user', {
   id: {
-    type: Sequelize.INTEGER, //es unda davgooglo ras nishnavs
+    type: Sequelize.INTEGER,
     autoIncrement: true,
     allowNull: false,
     primaryKey: true,
