f0889535452da01c45fb9d59b689164f159cbf82
Davit Kasradze <davit.kasradze23@gmail.com>
2023-12-25 15:39:24 +0400

    Lesson 324/error during adding item

--- routes/admin.js
+++ routes/admin.js
@@ -20,8 +20,6 @@ router.post(
     .isString()
     .isLength({min: 3})
     .trim(),
-  body('imageUrl')
-    .isURL(),
   body('price')
     .isFloat(),
   body('description')
@@ -41,8 +39,6 @@ router.post(
     .isString()
     .isLength({min: 3})
     .trim(),
-  body('imageUrl')
-    .isURL(),
   body('price')
     .isFloat(),
   body('description')
