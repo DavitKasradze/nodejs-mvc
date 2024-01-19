b38605f04f32f1d46de1f1fb358265983dddd766
root <root@ThomasTheCat>
2024-01-19 18:22:11 +0400

    mongodb update

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI =
-  `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@127.0.0.1:27017/${process.env.MONGO_DEFAULT_DATABASE}?authSource=admin`;
+  // `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@127.0.0.1:27017/${process.env.MONGO_DEFAULT_DATABASE}?authSource=admin`;
+  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.gmcrstj.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`
 
 const app = express();
 const store = new MongoDBStore({
